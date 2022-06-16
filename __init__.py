8fda054f631f434afa2054215bd1930da3b9704c
jgkang12345 <jgkang12345@gmail.com>
2022-06-16 23:48:06 +0900

    1.0 version end

--- __init__.py
+++ __init__.py
@@ -1,35 +1,34 @@
 import time
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from urllib.parse import urlparse, parse_qs
 import requests
 from selenium.webdriver.common.keys import Keys
 
-
 options = webdriver.ChromeOptions()
 options.add_argument('headless')
 driver = webdriver.Chrome('chromedriver.exe', options=options)
 
-def root_li_ret ():
-    root_select_ul = driver.find_element(By.CSS_SELECTOR,'#back > div > div.col-sm-12.col-md-12.col-xl-8.col-lg-8 > div:nth-child(2) > div.settle_div > div.jobName_div.col-sm-12.col-md-12.col-lg-12.col-xl-12 > ul')
+
+def root_li_ret():
+    root_select_ul = driver.find_element(By.CSS_SELECTOR,
+                                         '#back > div > div.col-sm-12.col-md-12.col-xl-8.col-lg-8 > div:nth-child(2) > div.settle_div > div.jobName_div.col-sm-12.col-md-12.col-lg-12.col-xl-12 > ul')
     select_li_list = root_select_ul.find_elements(By.TAG_NAME, 'li')
     return select_li_list
 
 
-def sub_li_ret ():
+def sub_li_ret():
     sub_tab_list = driver.find_element(By.CSS_SELECTOR,
                                        '#back > div > div.col-sm-12.col-md-12.col-xl-8.col-lg-8 > div:nth-child(2) > div.settle_div > div.select_div.col-sm-12.col-md-12.col-lg-12.col-xl-12 > ul')
     sub_tab_list_true = sub_tab_list.find_elements(By.TAG_NAME, 'li')
     return sub_tab_list_true
 
 
-
 if __name__ == '__main__':
 
     driver.get("https://dunfaoff.com/ranking.df")
 
-    while (True):
+    while True:
         try:
             select_li_list_size = len(root_li_ret())
 
@@ -67,30 +66,23 @@ if __name__ == '__main__':
                         imgUrl = imgList[index].get_attribute("src")
                         parse_result = urlparse(imgUrl)
                         requiredList = list(filter(lambda x: x != "",
-                                                   parse_result.path.replace("/df/servers/", "\\").replace("/characters/",
-                                                                                                           "\\").split(
+                                                   parse_result.path.replace("/df/servers/", "\\").replace(
+                                                       "/characters/",
+                                                       "\\").split(
                                                        "\\")))
                         item_list.append(
-                            {"ord_no": index, "server_id": requiredList[0], "ch_id": requiredList[1], "data_gb": data_gb,
+                            {"ord_no": index, "server_id": requiredList[0], "ch_id": requiredList[1],
+                             "data_gb": data_gb,
                              "data_detail_gb": data_detail_gb})
 
                     print('request_body: ', item_list)
                     headers = {'Content-Type': 'application/json; charset=utf-8'}
-                    response = requests.post("http://localhost:8080/python/insertRanker", json=item_list, headers=headers)
+                    response = requests.post("http://localhost:8080/python/insertRanker", json=item_list,
+                                             headers=headers)
 
                     print('response: ', response)
         except Exception as e:
             print(e)
             print("예외가 발생하였습니다 프로그램을 종료하고 5분후 다시 실행합니다")
 
-
-
         time.sleep(300)
-
-
-
-
-
-
-
-
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='dnfPythonModule',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='kangj',
+    author='kangj',
+    author_email='',
+    description=''
+)
